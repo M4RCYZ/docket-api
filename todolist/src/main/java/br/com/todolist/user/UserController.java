f7f602b56481cb9a95f2e4d615399c3e200c44d3
Marcus <marcusdevofc@gmail.com>
2025-08-20 10:09:33 -0300

    funcionalidade de cadastrar usuario

---
+++ todolist/src/main/java/br/com/todolist/user/UserController.java
@@ -0,0 +1,20 @@
+package br.com.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        return this.userRepository.save(user);
+    }
+}
